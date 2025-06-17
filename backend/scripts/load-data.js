631e2d65f91fe10466ac198dd43caecd202bf928
Kyle Chandler <kychandler26@gmail.com>
2025-06-17 11:52:12 +0100

    feat: implement database connection and release logic in data loading script

--- backend/scripts/load-data.js
+++ backend/scripts/load-data.js
@@ -13,13 +13,47 @@ import fs from 'fs';
 import { parse } from 'fast-csv';
 import dotenv from 'dotenv';
 import path from 'path';
-
+import { Pool } from 'pg';
+import  { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 // Main function to extract, transform, and load the data
 async function processAndLoadData() {
-  try {
-    
-  } finally {
+  let pool;
+  let client;
 
+  try {
+    // Open the new Pool connection to the PostgreSQL database
+    const pool = new Pool({
+      // Use environment variables for database connection details
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: 'postgres',
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT,
+    });
+    console.log('Connecting to the database...');
+    // Connect to the PostgreSQL database
+    client = await pool.connect();
+    console.log('Connected to the database successfully.');
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+    return;
+  } finally {
+    if (client) {
+      // Ensure the client is released back to the pool
+      client.release();
+      console.log('Database client released.');
+    }
+    if  (pool) {
+      // Ensure the pool is closed to free up resources
+      await pool.end();
+      console.log('Database connection pool closed.');
+    }
   }
 };
+
+
+processAndLoadData();
